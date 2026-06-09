21a3492d0302e34a57aff5f99ebcff1d8fbcc310
Rudreshkp <rudreshsekar@gmailcom>
2026-06-09 03:44:08 +0000

    Moved all gates into basic_gate folder

---
+++ basic_gate/nor_gate/nor_gate.v
@@ -0,0 +1,10 @@
+module nor_gate(
+    input a,
+    input b,
+    output y
+);
+
+assign y = ~(a | b);
+
+endmodule
+
---
+++ basic_gate/xor_gate/xor_gate.v
@@ -0,0 +1,10 @@
+module xor_gate(
+    input a,
+    input b,
+    output y
+);
+
+assign y = a ^ b;
+
+endmodule
+
